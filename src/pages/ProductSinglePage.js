89037b31d8d1d186f394a424bd016a87ae84899d
unknown <darpanpaudel1998@gmail.com>
2024-03-27 10:53:28 +0545

    sp

--- src/pages/ProductSinglePage.js
+++ src/pages/ProductSinglePage.js
@@ -5,7 +5,7 @@ const ProductSinglePage = ({ imageUrl, imageName }) => {
     <div>
       <h1>Single Image Page</h1>
       <img src={imageUrl} alt={imageName} />
-      <p>Image Names: {imageName}</p>
+      <p>Image Name: {imageName}</p>
     </div>
   );
 };
